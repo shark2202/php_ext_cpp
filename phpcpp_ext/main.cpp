3f468eff6e1722334b5b9398ab5a9fc19289d139
zd04 <zhjx2010dddd@gmail.com>
2019-02-13 17:22:54 +0800

    phpcpp的使用

--- phpcpp_ext/main.cpp
+++ phpcpp_ext/main.cpp
@@ -1,26 +1,141 @@
 #include <phpcpp.h>
+#include <iostream>
 
-/**
- *  tell the compiler that the get_module is a pure C function
- */
-extern "C" {
+//使用宏替换一下
+#define PHPCPP_EXTENSION()  extern "C" { PHPCPP_EXPORT void *get_module(); } PHPCPP_EXPORT void *get_module()
+
+//没有返回值的
+void myFunction()
+{
+    Php::out << "example output" << std::endl;
+}
+
+void evalPHP(Php::Parameters &params)
+{
+    Php::eval(params[0].stringValue());
+}
+
+Php::Value fnInCpp(Php::Parameters &p)
+{
+    return Php::Object("ClassInPhp");
+    Php::Value param = p[0];
+    return param * 2;
+}
+
+Php::Value multiply_by_two(Php::Parameters &params) {
+
+        // make sure the function was really called with at least one parameter
+        if (params.empty()) return nullptr;
+
+        // one parameter is passed to the function
+        Php::Value param = params[0];
+
+        // multiple the parameter by two
+        return param * 2;
+}
+
+Php::Value returnfunction(Php::Parameters &p)
+{
+    // //这个一个cpp的匿名函数
+    // Php::Function multiply_by_two([](Php::Parameters &params) -> Php::Value {
+
+    //     // make sure the function was really called with at least one parameter
+    //     if (params.empty()) return nullptr;
+
+    //     //Php::call("var_dump",params);
+
+    //     // one parameter is passed to the function
+    //     Php::Value param = params[0];
+
+    //     // multiple the parameter by two
+    //     return 10;
+    // });
+
+    // create the anonymous function
+    Php::Function multiply_by_two([](Php::Parameters &params) -> Php::Value {
+
+        // make sure the function was really called with at least one parameter
+        if (params.empty()) return nullptr;
+
+        // one parameter is passed to the function
+        Php::Value param = params[0];
+
+        // multiple the parameter by two
+        return param * 2;
+    });
+
+    // create an array
+    Php::Value array;
+    array[0] = 1;
+    array[1] = 2;
+    array[2] = 3;
+    array[3] = 4;
+
+    // call the user-space function
+    Php::Value f ("my_array_map");
+    Php::Value result = f(array, multiply_by_two);
+
+    Php::call("var_dump",result);
+
+    return 1;
+
+    Php::Function f1(fnInCpp);
+    return Php::Object("Closure",f1);
+    // return std::move(f1);
+
+    Php::Value r;
+    Php::call("var_dump",Php::Array(p[0]));
+
+    Php::call("var_dump",multiply_by_two);
+
+    r = Php::call("array_map",multiply_by_two,Php::Array(p[0]));
+
+    Php::call("var_dump",r);
+
+    return 0;
+}
+
+PHPCPP_EXTENSION()
+{
+    //使用静态变量常驻内存的
+    static Php::Extension extension("mytest", "1.0");
+
+    //cpp中定义的方法的名称，后面是php中定义的方法的名称
+    extension.add<myFunction>("newFunction");
+
+    //直接定义的命名空间的方法
+    extension.add<myFunction>("CppTest\\newFunction");
+
 
     /**
-     *  Function that is called by PHP right after the PHP process
-     *  has started, and that returns an address of an internal PHP
-     *  strucure with all the details and features of your extension
-     *
-     *  @return void*   a pointer to an address that is understood by PHP
-     */
-    PHPCPP_EXPORT void *get_module() 
-    {
-        // static(!) Php::Extension object that should stay in memory
-        // for the entire duration of the process (that's why it's static)
-        static Php::Extension extension("mytest", "1.0");
+    * 限定参数的类型
+    *   Php::Type::Null
+    *   Php::Type::Numeric
+    *   Php::Type::Float
+    *   Php::Type::Bool
+    *   Php::Type::Array
+    *   Php::Type::Object
+    *   Php::Type::String
+    *   Php::Type::Resource
+    *   Php::Type::Constant
+    *   Php::Type::ConstantArray
+    *   Php::Type::Callable
+    * 
+    *
+    */
+    extension.add<myFunction>("myFunction",{
+        Php::ByVal("a",Php::Type::Numeric),
+    });
 
-        // @todo    add your own functions, classes, namespaces to the extension
+    extension.add<evalPHP>("evalPhp",{
+        Php::ByVal("code",Php::Type::String),
+    });
 
-        // return the extension
-        return extension;
-    }
+    extension.add<returnfunction>("returnfunction");
+
+    extension.add<fnInCpp>("fnInCpp");
+
+    extension.add<multiply_by_two>("multiply_by_two");
+
+    return extension;
 }
---
+++ phpcpp_ext/test/phpcpp.php
@@ -0,0 +1,50 @@
+<?php 
+
+class ClassInPhp{};
+
+function my_array_map($array, $callback) {
+
+        var_dump($callback);
+
+        // initial result variable
+        $result = array();
+    
+        // loop through the array
+        foreach ($array as $index => $item) {
+    
+            // call the callback on the item
+            $result[$index] = $callback($item);
+            var_dump($result[$index]);
+        }
+    
+        // done
+        return $result;
+}
+
+
+var_dump(multiply_by_two(100));
+// exit;
+
+var_dump(is_callable('newFunction'));
+if(is_callable('newFunction')){
+    newFunction();
+}
+
+var_dump(is_callable("CppTest\\newFunction"));
+if(is_callable('CppTest\\newFunction')){
+    CppTest\newFunction();
+}
+
+// myFunction("daidhai");
+
+evalPHP("echo 'ok';");
+echo PHP_EOL;
+
+$a = fnInCpp(2);
+$a->ok = 100;
+var_dump($a);
+
+$f = returnfunction();
+var_dump(is_callable($f));
+var_dump($f instanceof Closure);
+// var_dump($f(3));
